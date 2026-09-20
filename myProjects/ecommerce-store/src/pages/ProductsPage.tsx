8f0bbd694b287f45bbbdf5aa0e1c3d44df0cfcc1
Rakesh Ranjan <rakeshmodi034@gmail.com>
2026-09-20 21:17:34 +0530

    code added

--- myProjects/ecommerce-store/src/pages/ProductsPage.tsx
+++ myProjects/ecommerce-store/src/pages/ProductsPage.tsx
@@ -1,8 +1,28 @@
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../store/hooks";
+import { fetchProductsStart } from "../store/slices/productSlice";
 
 const ProductsPage = () =>{
+    const dispatch = useAppDispatch();
+    const {products, loading, error} = useAppSelector((state) => state.products)
+
+    useEffect(()=>{
+        dispatch(fetchProductsStart());
+    },[])
+    if(loading){
+        return <p>Loading...</p>
+    }
+    if(error !== null){
+        return <p>{error}</p>
+    }
     return (
         <div>
-            <h3>ProductsPage</h3>
+             {products.map((product) => (
+                <div key={product.id}>
+                    <h3>{product.title}</h3>
+                    <p>${product.price}</p>
+                </div>
+            ))}
         </div>
     )
 }
--- myProjects/ecommerce-store/src/store/index.ts
+++ myProjects/ecommerce-store/src/store/index.ts
@@ -1,9 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
-import productReducer from './slices/productSlice'
+import productReducer from './slices/productSlice';
+import createSagaMiddleware from 'redux-saga'
+import rootSaga from "./sagas/rootSaga";
+const sagaMiddleware = createSagaMiddleware();
 export const store = configureStore({
     reducer:{
         products: productReducer
-    }
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(sagaMiddleware)
 })
+
+sagaMiddleware.run(rootSaga);
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch
